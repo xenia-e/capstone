16b72ebed79399fb7f5f081871699099f997eb58
Xenia Sokol <sokol.xenia@gmail.com>
2021-11-07 17:59:46 -0500

    medals layer added to map

--- map/static/js/olympicMap.js
+++ map/static/js/olympicMap.js
@@ -17,7 +17,7 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-	center: [40.7, -94.5],
+	center: [40.21, 2.86],
 	zoom: 3,
 	layers: [streets]
 });
@@ -36,7 +36,7 @@ let averageWinner = new L.LayerGroup();
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
   "Olympic Countries": olympicCountries,
-  "Medals": olympicMedals,
+  "Medals won by countries": olympicMedals,
   "Average Athlete": averageWinner,
 };
 
@@ -54,28 +54,37 @@ d3.json("./static/js/mapData.geojson").then(function(data) {
     return {
       opacity: 1,
       fillOpacity: 0.5,
-      fillColor: getColor(feature.properties.total_Games),
+      fillColor: getColor(feature.properties.Games),
       color: "#000000",
-      radius: getRadius(feature.properties.total_Games),
+      radius: getRadius(feature.properties.Games),
       stroke: true,
       weight: 0.5
     };
   }
 
   // This function determines the color of the marker based on games participated.
-  function getColor(total_Games) {
-    if (total_Games > 10) {
+  function getColor(Games) {
+    if (Games > 40) {
       return "#ea2c2c";
     }
-    if (total_Games > 5) {
+    if (Games > 30) {
       return "#ea822c";
+    }
+    if (Games > 20) {
+      return "#ee9c00";
+    }
+    if (Games > 10) {
+      return "#eecc00";
+    }
+    if (Games > 5) {
+      return "#d4ee00";
     }
     return "#98ee00";
   }
 
   // This function determines the radius of the country marker based on games participated.
-  function getRadius(total_Games) {
-    return total_Games * 0.5;
+  function getRadius(Games) {
+    return Games * 0.7;
   }
 
   // Creating a GeoJSON layer with the retrieved data.
@@ -89,10 +98,96 @@ d3.json("./static/js/mapData.geojson").then(function(data) {
      // We create a popup for each circleMarker to display the magnitude and location of the earthquake
      //  after the marker has been created and styled.
      onEachFeature: function(feature, layer) {
-      layer.bindPopup("<h4>" + feature.properties.name + "</h4><br>Games participated: " + feature.properties.total_Games);
+      layer.bindPopup("<h4>" + feature.properties.CountryName + "</h4>Games participated: " + feature.properties.Games + '</br> Medals earned: ' + feature.properties.totalWins + '</br> Total athletes participated: ' + feature.properties.Athletes);
     }
   }).addTo(olympicCountries);
 
   // Then we add the earthquake layer to our map.
   olympicCountries.addTo(map);
+
+  // Here we create a legend control object.
+    let legend = L.control({
+      position: "bottomright"
+    });
+  
+    // Then add all the details for the legend
+    legend.onAdd = function() {
+      let div = L.DomUtil.create("div", "info legend");
+      //labels = ['<strong> Games participaded </strong>'];
+  
+      const games = ['<5', '>5', '>10', '>20', '>30', '>40'];
+      const colors = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#ea822c",
+        "#ea2c2c"
+      ];
+  
+  // Looping through our intervals to generate a label with a colored square for each interval.
+    for (var i = 0; i < games.length; i++) {
+      div.innerHTML +=        
+        "<i style='background: " + colors[i] + "'></i>&nbsp;&nbsp;" +
+        games[i] + "</br>";
+      
+      }
+      return div;
+    };
+  
+    // Finally, we our legend to the map.
+    legend.addTo(map);
 });
+
+// ADDING MEDALS LAYER
+
+d3.json("./static/js/mapData.geojson").then(function(data) {
+
+  // This function returns the style data for each of the country we plot on
+  // the map. We pass the to games participated into two separate functions
+  // to calculate the color and radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 0.75,
+      fillColor: getColor2(feature.properties.totalWins),
+      color: "#000000",
+      radius: getRadius2(feature.properties.totalWins),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+// This function determines the radius of the country marker based on games participated.
+  function getRadius2(totalWins) {
+    if (totalWins === 0) {
+      return 4;
+    }
+    return totalWins*0.01;
+  }
+  function getColor2(totalWins){
+    if (totalWins === 0) {
+      return 'grey';
+    }
+    return "#bf9b30"
+  }
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+    	// We turn each feature into a circleMarker on the map.
+    	pointToLayer: function(feature, latlng) {
+      		return L.circleMarker(latlng);
+        },
+    style: styleInfo,
+     onEachFeature: function(feature, layer) {
+      layer.bindPopup('<h4>' + feature.properties.CountryName + '</h4><hr> Medals earned: ' + feature.properties.totalWins +
+       '</br><hr> Gold medals: ' + feature.properties.Medal_Gold +
+       '</br> Silver medals: ' + feature.properties.Medal_Silver +
+       '</br> Bronze medals: ' + feature.properties.Medal_Bronze
+       );
+    }
+  }).addTo(olympicMedals);
+
+  // Then we add the earthquake layer to our map.
+  //olympicMedals.addTo(map);
+
+});
+
