c80eb9075c237b00021ed731b76c2b59fb4b94d6
Soco Hernandez <85274353+chocoplace@users.noreply.github.com>
2021-11-14 16:28:47 -0500

    Updated SQL to export Table
    I have joined the two steps before to just one and you can find the CSV table on resources as well under final_olympics.

--- Database Schema/Completedschema.sql
+++ Database Schema/Completedschema.sql
@@ -31,35 +31,27 @@ CREATE TABLE Games(
 );
 
 -- join the tables using PgAdmin 
-SELECT * from athletes as a
-INNER JOIN games as g on g.games = a.games;
+SELECT a.ID, 
+	   a.Name, 
+	   a.Sex, 
+	   a.Age, 
+	   a.Height, 
+	   a.Weight, 
+	   a.Team, 
+	   a.NOC,
+	   a.Games, 
+	   a.Sport,
+	   a.Event,
+	   a.Medal,
+	   a.Win,
+	   a.BMI,
+	   a.Team_wins,
+	   a.Win_per_member  
+INTO final_olympics
+FROM athletes_medals as a
+INNER JOIN games as g 
+ON g.games = a.games;
 
+SELECT * FROM final_olympics;
 
---Create a new table from the joined tables--
-
-CREATE TABLE athletes_games as
-(
-SELECT
-    a.ID,
-	a.Name,
-	a.Sex,
-	a.Age,
-	a.Height,
-	a.Weight,
-	a.Team,
-	a.NOC,
-	a.Games,
-	a.Sport,
-	a.Event,
-	a.Medal,
-	a.Win,
-	a.BMI,
-	a.Team_wins,
-	a.Win_per_member,
-	g.Year,
-	g.Season,
-	g.City
-FROM athletes_medals AS a
-INNER JOIN Games AS g
-    ON g.games = a.games
-);
+--- Once the table is under TABLE select import/export with add the address file and .CSV and export the table. 
